2c4337bc9df94570c7724cdaf57bd213d37269ab
Piotr Rutkowski <rutkowski.2387@gmail.com>
2023-05-03 17:30:19 +0200

    Api POST request
    Post on form submit with files upload before

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -13,16 +13,16 @@ interface FormData {
   city: string
   address: string
   contact_preference: string[]
-  photos: {
-    url: string
-    name: string
-  }
+  photos: Array<string | Blob>
   project_type: string
   project_level: string
   additional_info: string
-  construction: string[]
+  contractor: boolean
   package: Packages | undefined
 }
+
+type InquiryPayload = Omit<FormData, "photos"> & { photos: number[] }
+
 const defaultData = {
   name: "",
   email: "",
@@ -30,16 +30,15 @@ const defaultData = {
   city: "",
   address: "",
   contact_preference: [],
-  photos: {
-    url: "",
-    name: "",
-  },
+  photos: undefined,
   project_type: "",
   project_stage: "",
   additional_info: "",
-  contractor: [],
+  contractor: undefined,
   package: undefined,
 }
+
+// { phone: true, online_meeting: false} as Record<string, boolean>
 
 const Form: React.FC = () => {
   const FormMethods = useForm<FormData>({ defaultValues: defaultData })
@@ -47,18 +46,36 @@ const Form: React.FC = () => {
   const { handleSubmit } = FormMethods
 
   const onFormSubmit = async (data: FormData) => {
+    const token =
+      "40c08a534448a91544103e934fad533513a6785a07d8a9773f4a3754e8decf6dd1190804437971b58854a3847c9c2614e90231e3392288fb14e8372411bebbdb74df58c4708f0065b0d600488bb6327187c910a9a80d2f2976c3910e7f571f597bbd12a760f4e0d8e7677c9bc95557505ad9ce42ab15cb13ab95fbbbfa1bddf1"
+    let formData = new FormData()
+    Object.values(data.photos).forEach((photo: string | Blob) =>
+      formData.append("files", photo)
+    )
+
     try {
-      const response = await axios.post(
-        "https://strapi-km.herokuapp.com/api/inquiries",
-        {
-          headers: {
-            Authorization: `Bearer {40c08a534448a91544103e934fad533513a6785a07d8a9773f4a3754e8decf6dd1190804437971b58854a3847c9c2614e90231e3392288fb14e8372411bebbdb74df58c4708f0065b0d600488bb6327187c910a9a80d2f2976c3910e7f571f597bbd12a760f4e0d8e7677c9bc95557505ad9ce42ab15cb13ab95fbbbfa1bddf1}`,
-          },
-        }
-      )
-      console.log(response)
+      const res = await axios({
+        method: "POST",
+        url: "https://strapi-km.herokuapp.com/api/upload",
+        data: formData,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      const photoIds: number[] = res.data.map((e: { id: number }) => e.id)
+      let dataCopy: InquiryPayload = { ...data, photos: photoIds }
+
+      const response = await axios({
+        method: "POST",
+        url: "https://strapi-km.herokuapp.com/api/inquiries",
+        data: { data: dataCopy },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      console.log("Inquiry", response.data)
     } catch (error) {
-      console.error(error)
+      console.log(error)
     }
   }
 
