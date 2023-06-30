e4d0750da95d9b4e5fb8ec67b02a8ab8549ffa72
Piotr Rutkowski <rutkowski.2387@gmail.com>
2023-06-30 11:31:43 +0200

    Add useParams in Administraton Panel
    Target specific inquiry by ID to display full description and images submitted by clients

--- src/components/administration/Inquiry.tsx
+++ src/components/administration/Inquiry.tsx
@@ -1,77 +1,47 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
 import { XMarkIcon } from "@heroicons/react/24/outline"
 
 interface PropsType {
-  id: number
-  item: any
-  deleteInquiry: any
+  id?: number
+  item?: any
+  deleteInquiry?: any
 }
 
 const Inquiry: React.FC<PropsType> = ({ item, id, deleteInquiry }) => {
   const backendUrl = "https://strapi-km.herokuapp.com"
-  const [isDropdown, setIsDropdown] = useState(false)
-  const openAdditionalInfo = (id: any) => {
-    setIsDropdown((prev) => !prev)
-  }
-
-  const buttonText = isDropdown ? "Close" : "Open"
 
   return (
     <tr
       data-testid={`inquiry-${item.id}`}
       key={item.id}
-      className="text-xs text-gray-900 text-center border content-center"
+      className="text-sm font-thin text-gray-900 text-center border content-center"
     >
-      <td className="text-xl text-indigo-900">{id}</td>
-      <td className="p-4">{item.name}</td>
+      <td className="text-xl font-normal text-indigo-900 p-5">{id}</td>
+
+      <td>{item.name}</td>
       <td
-        className="cursor-pointer hover:text-indigo-300 mr-1 font-light underline underline-offset-4 decoration-indigo-800"
+        className="cursor-pointer hover:text-indigo-300 mr-1 underline decoration-1 underline-offset-4 decoration-indigo-800"
         onClick={() => (window.location.href = `mailto:${item.email}`)}
       >
         {item.email}
       </td>
-      <td className="p-5">{item.phone}</td>
-      <td className="p-5">{item.city}</td>
-      <td className="p-5">{item.address}</td>
-      <td className="p-5">{item.contact_preference}</td>
-      <td className="p-5">{item.project_level}</td>
-      <td className="p-5">{item.project_type}</td>
-      <td className="p-5">{item.contractor ? "Yes" : "No"}</td>
+      <td>{item.phone}</td>
+      <td>{item.city}</td>
+      {/* <td>{item.address}</td>
+      <td>{item.contact_preference}</td>
+      <td>{item.project_level}</td> */}
+      <td>{item.project_type}</td>
+      <td>{item.contractor ? "Interested" : "Not interested"}</td>
       <td>
-        <button
-          className={`bg-indigo-900 text-gray-50 cursor-pointer transition-all transform  hover:font-bold mx-auto h-5 w-10 border ${
-            isDropdown ? "" : " text-indigo-900 bg-transparent"
-          }`}
-          onClick={() => openAdditionalInfo(id)}
-        >
-          {buttonText}
+        <button className="text-indigo-900 cursor-pointer transition-all transform  hover:font-semibold hover:text-white hover:bg-indigo-900  mx-auto h-6 w-12 border border-gray-300">
+          <Link to={`/admin/${id}`}>Open</Link>
         </button>
-        <div
-          className={`transition duration-300 ease-in-out h-3/6 left-0 mt-8 z-50 p-2 font-light bg-gray-50 text-black absolute  ${
-            isDropdown ? "" : "opacity-0 invisible"
-          }`}
-        >
-          <div className="grid grid-rows-2 grid-cols-2 gap-10">
-            <div className="row-span-2 text-base text-justify leading-7 font-extralight">
-              {item.additional_info}
-            </div>
-            <div className="flex">
-              {item.photos.data?.map((photo: any, id: number) => (
-                <img
-                  key={item.id}
-                  className="w-3/6  mx-auto"
-                  src={`${backendUrl}${photo.attributes.url}`}
-                  alt=""
-                />
-              ))}
-            </div>
-          </div>
-        </div>
       </td>
       <td>
         <XMarkIcon
           onClick={() => deleteInquiry(id)}
-          className="transition-all transform mx-auto bg-indigo-900 text-white h-5 w-5 hover:scale-110 hover:border hover:border-indigo-900 hover:bg-indigo-100 hover:text-indigo-900 cursor-pointer"
+          className="transition-all transform mx-auto bg-indigo-900 text-white h-6 w-6 hover:scale-110 hover:border hover:border-indigo-900 hover:bg-indigo-100 hover:text-indigo-900 cursor-pointer"
         />
       </td>
     </tr>
