0888dbc753780789972f61e58a9d418d4d2f55bf
Piotr Rutkowski <rutkowski.2387@gmail.com>
2023-08-01 13:17:57 +0200

    Chat Logo Styling

--- src/components/liveChat/Logo.tsx
+++ src/components/liveChat/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/outline"
 const Logo = () => {
   return (
-    <div className="text-indigo-900 flex gap-2 text-center text-xl font-medium mb-2">
+    <div className="text-indigo-900 flex gap-2 text-center text-xl font-medium mb-2 p-4">
       <ChatBubbleLeftRightIcon className="h-6" />
       Support Chat
     </div>
