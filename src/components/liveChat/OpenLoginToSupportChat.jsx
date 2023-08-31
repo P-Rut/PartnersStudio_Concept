1b75cc81c7c8849a8983509df46ddfabc85982c9
Piotr Rutkowski <rutkowski.2387@gmail.com>
2023-08-31 14:08:48 +0200

    Styling bug

--- src/components/liveChat/OpenLoginToSupportChat.jsx
+++ src/components/liveChat/OpenLoginToSupportChat.jsx
@@ -34,11 +34,11 @@ const OpenLoginToSupportChat = () => {
   return (
     <>
       {!!open && (
-        <div className="transition ease-out duration-300 fixed bottom-20 right-5 flex flex-col h-3/5 w-1/4 items-center bg-white  p-6 rounded-md border-2 border-indigo-900 ">
-          <h1 className="font-extralight mb-4 mt-4 px-6">
+        <div className="transition ease-out duration-300 fixed bottom-20 right-5 flex flex-col h-3/5 w-1/4 items-center bg-white  p-4 rounded-md border-2 border-indigo-900 ">
+          <h1 className="font-extralight mb-4 mt-4 px-6 leading-5">
             If you need help feel free to contact our support team.
           </h1>
-          <h2 className="text-xs font-thin mb-10 px-6">
+          <h2 className=" text-sm font-thin mb-5 px-6">
             To start chatting provide us with username below.
           </h2>
           <form onSubmit={handleSubmit}>
@@ -50,11 +50,11 @@ const OpenLoginToSupportChat = () => {
               className="block w-full"
             />
             {usernameExists && (
-              <p className="text-red-500 mb-4 text-xs">
-                Username is taken. Try another.
+              <p className="text-red-500 mb-2 mt-2 text-xs justify-center flex">
+                Username is taken. <br /> Please try another.
               </p>
             )}
-            <button className="text-white  bg-indigo-900 block w-full p-2 hover:opacity-50">
+            <button className="text-white mt-2  bg-indigo-900 block w-full p-2  hover:opacity-50">
               Create
             </button>
           </form>
